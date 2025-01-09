16d48c5eaf2648dfc6c286ed5711c057be77027a
Jacob Adams big-boi <jacob.adams121202@gmail.com>
2025-01-08 16:57:57 -0800

    refactor Maze class to implement method stubs for cell creation and drawing

--- mazy.py
+++ mazy.py
@@ -1,7 +1,8 @@
 from cell import Cell
 
+
 class Maze:
-  def __init__(
+    def __init__(
         self,
         x1,
         y1,
@@ -11,30 +12,20 @@ class Maze:
         cell_size_y,
         win,
     ):
-    self.x1 = x1
-    self.y1 = y1
-    self.num_rows = num_rows
-    self.num_cols = num_cols
-    self.cell_size_x = cell_size_x
-    self.cell_size_y = cell_size_y
-    self.win = win
-    self._create_cells()
-    
-  def _create_cells(self):
-    self._cells = []
-    for col in range(self.num_cols):
-      column = []
-      for row in range(self.num_rows):
-        cell = Cell(
-          self.x1 + col * self.cell_size_x,
-          self.y1 + row * self.cell_size_y,
-          self.cell_size_x,
-          self.cell_size_y,
-          self.win
-        )
-        cell._draw_cell()
-        column.append(cell)
-      self._cells.append(column)
-  
-  def _draw_cell(self,i,j):
-    
+        self.x1 = x1
+        self.y1 = y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.win = win
+        self._create_cells()
+
+    def _create_cells(self):
+        pass
+
+    def _draw_cell(self, i, j):
+        pass
+
+    def _animate(self):
+        pass
